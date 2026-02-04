8d79c64456411d12650df3434cc797c688e5fa73
gabrielfruet <gabrielfruet538@gmail.com>
2026-02-04 10:03:10 -0300

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import remarkMath from 'remark-math';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gabrielfruet.github.io',
-  base: '/website',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   },
